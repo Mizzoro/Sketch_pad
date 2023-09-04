063c13da0877f59b447a46d1aa585fed93cba328
Mizzoro <misbah.mukith@outlook.com>
2023-09-04 10:01:08 +0100

    Added functionality to buttons and grid
    need to design and polish

--- js/script.js
+++ js/script.js
@@ -1,26 +1,66 @@
-// create function to create divs with classes
+// create function to create divs, include color change event listener
 // create grid and append divs to it
+// reset button
+// change grid size
 
 const grid = document.querySelector('.grid');
-let gridSize = 8;
+const resetBtn = document.querySelector('.clear');
+const sizeInput = document.querySelector('input');
+const applyBtn = document.querySelector('.apply')
+const gridText = document.querySelector('.grid_size')
+let currentGridSize = 8;
+
 
 const createDiv = (size) => {
     const div = document.createElement('div');
     div.classList.add('box');
     div.style.width = `${size}px`;
     div.style.height = `${size}px`;
-
+    div.addEventListener('mouseover', e => {
+        e.target.style.backgroundColor = 'blue';
+    } )
 
     return div
 }
 
-const addDivs = (gridSize) => {
+const createGrid = (gridSize) => {
     for (let i = 0; i < gridSize; i++) {
         for (let j = 0; j < gridSize; j++) {
             grid.appendChild(createDiv(grid.clientWidth / gridSize));
         }
     }
+
 }
 
-console.log(grid.clientWidth )
-addDivs(gridSize);
+const removeGridDivs = () => {
+    while(grid.firstChild) {
+        grid.removeChild(grid.firstChild)
+    }
+}
+
+const newGrid = () => {
+    removeGridDivs();
+    createGrid(sizeInput.value);  
+}
+
+const resetGrid = () => {
+    removeGridDivs();
+    createGrid(currentGridSize);
+}
+
+
+applyBtn.addEventListener('click', () => {
+    if (currentGridSize != sizeInput.value) {
+        currentGridSize = sizeInput.value;
+        newGrid();
+        gridText.textContent = `${currentGridSize}x${currentGridSize}`
+    }
+});
+
+resetBtn.addEventListener('click', () => {
+    resetGrid();
+});
+
+
+createGrid(currentGridSize);
+
